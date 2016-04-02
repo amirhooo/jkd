07121153af86d089291b7a3b503b7cc6ad30352d
amirhooo <amirwh@30mail.ir>
2016-04-02 11:17:46 +0430

    Added files via upload

---
+++ plugins/locklink.lua
@@ -0,0 +1,15 @@
+local function run(msg, matches)
+ if not is_sudo(msg) then 
+delete_msg(msg.id, ok_cb, false)
+end       
+end
+return {
+patterns = {
+    "(.*)[Hh][Tt][Tt][Pp][Ss]://",
+    "[Hh][Tt][Tt][Pp][Ss]://(.*)",
+    "(.*)[Hh][Tt][Tt][Pp]://",
+    "[Hh][Tt][Tt][Pp]://(.*)",
+    
+}, 
+run = run
+}
